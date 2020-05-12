f5d90bb839e00b5d452162684a2a6a9d6045144f
jordanforbes <jordforb@gmail.com>
2020-05-12 17:06:50 -0400

    created generation

--- randomword/views.py
+++ randomword/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils.crypto import get_random_string
 
-# Create your views here.
+def main(request):
+    if 'counter' in request.session:
+        request.session['counter']= request.session['counter']+1
+    else:
+        request.session['counter']=0
+    print('session counter'+ str(request.session['counter']))
+    print('random: '+get_random_string(length=14))
+    
+    context={
+        'counter': request.session['counter'],
+        'randword': get_random_string(length = 14)
+    }
+    return render(request, 'index.html', context)
