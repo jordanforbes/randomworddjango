62b752d5f5fe9996ff61a4ba8c3b090fb0d819b2
jordanforbes <jordforb@gmail.com>
2020-05-13 10:40:46 -0400

    split into paths

--- randomword/urls.py
+++ randomword/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main)
+    path('', views.main),
+    path('random_word', views.randomword)
 ]
--- randomword/views.py
+++ randomword/views.py
@@ -2,25 +2,23 @@ from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 
 def main(request):
-    request.session['randword'] = get_random_string(length=14)
     
-    if 'counter' in request.session:
-        request.session['counter']= request.session['counter']+1
-    else:
+    if 'counter' not in request.session:
         request.session['counter']=0
+    
+    context={
+        'randword': get_random_string(length = 14)
+    }
+        
     print('session counter'+ str(request.session['counter']))
     print('random: '+ request.session['randword'])
     
-
     
-    # context={
-    #     'counter': request.session['counter'],
-    #     'randword': get_random_string(length = 14)
-    # }
-    
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
 
 def randomword(request):
-    request.session['counter']= request.session['counter']+1
-    request.session['randword'] = get_random_string(length=14)
-    return redirect()
+    request.session['counter']+=1
+    request.session['randword']=get_random_string(length=14)
+    print(request.POST)
+    
+    return redirect('/')
