6be8979a1bedab983d0242dcaa371d4ab1e7d5ff
jordanforbes <jordforb@gmail.com>
2020-05-13 07:34:53 -0400

    created second method

--- randomword/views.py
+++ randomword/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils.crypto import get_random_string
 
 def main(request):
+    request.session['randword'] = get_random_string(length=14)
+    
     if 'counter' in request.session:
         request.session['counter']= request.session['counter']+1
     else:
         request.session['counter']=0
     print('session counter'+ str(request.session['counter']))
-    print('random: '+get_random_string(length=14))
+    print('random: '+ request.session['randword'])
     
-    context={
-        'counter': request.session['counter'],
-        'randword': get_random_string(length = 14)
-    }
-    return render(request, 'index.html', context)
+
+    
+    # context={
+    #     'counter': request.session['counter'],
+    #     'randword': get_random_string(length = 14)
+    # }
+    
+    return render(request, 'index.html')
+
+def randomword(request):
+    request.session['counter']= request.session['counter']+1
+    request.session['randword'] = get_random_string(length=14)
+    return redirect()
